1564c1d2ba3753098f98262f27747c6fe5dd2c43
cafel176 <1457520405@qq.com>
2026-02-04 10:09:01 +0800

    更新tutorial

--- mod-tool/i18n-helper.js
+++ mod-tool/i18n-helper.js
@@ -132,7 +132,7 @@
                         <div>
                             <div style="font-weight:700;margin-bottom:4px;">i18n 资源加载失败</div>
                             <div>你正在以 <code style="color:#fcd34d;">file://</code> 方式打开页面，浏览器会拦截对本地 <code style="color:#fcd34d;">i18n/*.json</code> 的 <code style="color:#fcd34d;">fetch</code>。</div>
-                            <div>请使用本地 HTTP 服务打开（例如运行 <code style="color:#fcd34d;">mod-tool/打开-mod编辑器.bat</code>，然后访问 <code style="color:#fcd34d;">http://127.0.0.1:4173/</code>）。</div>
+                            <div>请使用本地 HTTP 服务打开（例如运行 <code style="color:#fcd34d;">mod-tool/打开-mod编辑器.bat</code>，然后访问 <code style="color:#fcd34d;">http://127.0.0.1:4174/</code>）。</div>
                         </div>
                         <button type="button" aria-label="close" style="cursor:pointer;background:transparent;border:0;color:#fbbf24;font-weight:700;font-size:14px;line-height:1;">×</button>
                     </div>
